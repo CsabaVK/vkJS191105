1cca9f4b06fa7c88f8c4e2e97b1ba458b52b5363
CsabaVK <56678429+CsabaVK@users.noreply.github.com>
2019-11-05 08:47:34 +0100

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,20 @@
+/*var a = " https://bmszc-verebély.e-kreta.hu/Orarend"
+var b = encodeURI(a);
+var c = decodeURI(b);
+
+var d = encodeURIComponent(b + "?" + a);
+document.write("<h1>" + b + "?" + d + "</h1>");*/
+
+/*function f(){
+  document.write("It's do something")
+}
+var f2 = f;
+
+console.log(f2());*/
+
+
+
+/*var t = [1,12];
+var t2 = t;
+t[1] = 23;
+console.log(t);*/
